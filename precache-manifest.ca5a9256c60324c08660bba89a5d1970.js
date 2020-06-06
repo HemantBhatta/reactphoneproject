63d7dab67194bb1511ec0f767e612213a8134d9c
HemantBhatta <hemub70@gmail.com>
2020-06-06 10:29:11 +0545

    Updates

---
+++ precache-manifest.ca5a9256c60324c08660bba89a5d1970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c675c68de92cd868d6a225da7444d2fe",
+    "url": "/reactphoneproject/index.html"
+  },
+  {
+    "revision": "0bd1f6d2893d449c47d3",
+    "url": "/reactphoneproject/static/css/main.777247ae.chunk.css"
+  },
+  {
+    "revision": "8636251d93acc4afa7b4",
+    "url": "/reactphoneproject/static/js/2.cc06ce76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactphoneproject/static/js/2.cc06ce76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd1f6d2893d449c47d3",
+    "url": "/reactphoneproject/static/js/main.286a9eff.chunk.js"
+  },
+  {
+    "revision": "5fe9b73717506ff23d4a",
+    "url": "/reactphoneproject/static/js/runtime-main.22cf237a.js"
+  }
+]);
